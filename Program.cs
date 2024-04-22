b463c70a335092774b51195806d30c999a17549c
Mailaf Tewodros <111310925+ekimwill@users.noreply.github.com>
2024-04-22 23:01:23 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,78 +2,7 @@ using Bank;
 using Spectre.Console;
 
 
-
-
-var isrunning = true;
-
-while (isrunning)
-{
-
-    var options = AnsiConsole.Prompt(
-        new SelectionPrompt<MenuOPtion>()
-        .Title("This is Mikes bank")
-        .AddChoices(
-            MenuOPtion.CreateAccount,
-            MenuOPtion.Deposit,
-            MenuOPtion.withdraw,
-            MenuOPtion.SeeAllAccounts,
-            MenuOPtion.DeleteAccount,
-            MenuOPtion.Quit));
+UserInterface.MainMenu();
 
 
 
-    switch (options)
-    {
-        case MenuOPtion.CreateAccount:
-            ProductService.CreateProduct();
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
-            Console.Clear();
-            break;
-        case MenuOPtion.DeleteAccount:
-            ProductService.DeleteProduct();
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
-            Console.Clear();
-            break;
-        case MenuOPtion.Deposit:
-            ProductService.Deposit();
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
-            Console.Clear();
-            break;
-        case MenuOPtion.withdraw:
-            ProductService.Withdraw();
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
-            Console.Clear();
-            break;
-        case MenuOPtion.SeeAllAccounts:
-            ProductService.SeeAllAccounts();
-            Console.WriteLine("Press any key to continue");
-            Console.ReadLine();
-            Console.Clear();
-            break;
-        case MenuOPtion.Quit:
-            isrunning = false;
-            break;
-    }
-}
-
-
-
-
-
-
-
-
-
-enum MenuOPtion
-{
-    CreateAccount,
-    Deposit,
-    withdraw,
-    SeeAllAccounts,
-    DeleteAccount,
-    Quit
-}
